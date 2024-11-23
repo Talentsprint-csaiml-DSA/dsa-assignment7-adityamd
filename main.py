6836f3dc74e0ee0d866f367634efce6123fa64c8
Aditya Tripathi <aditya.t2210@gmail.com>
2024-11-24 02:15:55 +0530

    Solution

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def longest_common_subsequence():
-    # ToDo 
+def lcs(X, Y, m, n):
+ 
+    if m == 0 or n == 0:
+       return 0
+    elif X[m-1] == Y[n-1]:
+       return 1 + lcs(X, Y, m-1, n-1)
+    else:
+       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n))
+    
+def longest_common_subsequence(X, Y):
+    return lcs(X, Y, len(X), len(Y))
